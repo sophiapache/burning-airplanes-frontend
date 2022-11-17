19b13c29ea307daabd83ba29d3efe6879dd35cbc
Sophia Pache <sophpache@gmail.com>
2022-11-17 13:43:14 +1100

    added some styling

--- src/components/Homepage/FlightDisplay.jsx
+++ src/components/Homepage/FlightDisplay.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 const FlightDisplay = (props) => {
     return (
         <div className='flight-info-container'>
-            <div>{ props.flights.map((f) => <p key={ f.id }>Date: { f.date }</p>) }</div>
-            <div>{ props.flights.map((f) => <p><Link to ={`/flights/${ f.id }`}>Flight ID: { f.id }</Link></p>) }</div>
-            <div> { props.flights.map((f) => <p key={ f.id }>Origin: { f.origin }</p>) }</div>
-            <div>{ props.flights.map((f) => <p key={ f.id }>Destination: { f.destination }</p>) }</div>
-            <div>{ props.flights.map((f) => <p key={ f.id }>Airplane: { f.airplane_id }</p>) }</div>
+            <div><h3>Date:</h3>{ props.flights.map((f) => <p key={ f.id }>{ f.date }</p>) }</div>
+            <div><h3>Flight ID:</h3>{ props.flights.map((f) => <p><Link to ={`/flights/${ f.id }`}>{ f.id }</Link></p>) }</div>
+            <div><h3>Origin:</h3>{ props.flights.map((f) => <p key={ f.id }>{ f.origin }</p>) }</div>
+            <div><h3>Destination:</h3>{ props.flights.map((f) => <p key={ f.id }>{ f.destination }</p>) }</div>
+            <div><h3>Airplane ID:</h3>{ props.flights.map((f) => <p key={ f.id }>{ f.airplane_id }</p>) }</div>
         </div>
     );
 };
